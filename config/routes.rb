67523d2e01b89d482596dc56532f73e67d0f3470
Florian Herlicq <fherlicq@gmail.com>
2017-11-27 16:53:17 +0100

    added nanny offers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'nanny_offers/index'
+
+  get 'nanny_offers/create'
+
+  get 'nanny_offers/new'
+
+  get 'nanny_offers/edit'
+
+  get 'nanny_offers/show'
+
+  get 'nanny_offers/update'
+
+  get 'nanny_offers/destroy'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
