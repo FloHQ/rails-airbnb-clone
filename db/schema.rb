a83cfd22eab1fc2e92b3230fc6aa32c15414c319
Florian Herlicq <fherlicq@gmail.com>
2017-11-27 17:58:38 +0100

    db change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127153850) do
+ActiveRecord::Schema.define(version: 20171127155042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 20171127153850) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "nanny_daily_offers", force: :cascade do |t|
+    t.bigint "nanny_offer_id"
+    t.bigint "booking_id"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["booking_id"], name: "index_nanny_daily_offers_on_booking_id"
+    t.index ["nanny_offer_id"], name: "index_nanny_daily_offers_on_nanny_offer_id"
   end
 
   create_table "nanny_offers", force: :cascade do |t|
@@ -56,5 +66,7 @@ ActiveRecord::Schema.define(version: 20171127153850) do
   end
 
   add_foreign_key "bookings", "users"
+  add_foreign_key "nanny_daily_offers", "bookings"
+  add_foreign_key "nanny_daily_offers", "nanny_offers"
   add_foreign_key "nanny_offers", "users"
 end
