13cc7e8408b91c8ac298c967e049774edc05b89e
Florian Herlicq <fherlicq@gmail.com>
2017-11-28 14:45:14 +0100

    modified controllers to prepare the configuration of the forms for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
-  # def index
+  # # def index
+  # # end
+
+  # def show
+  #   @user = User.find(params[:id])
   # end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  # def new
+  #   @user = User.new
+  # end
 
-  def new
-    @user = User.new
-  end
+  # def create
+  #   @user = User.new(params[:user])
+  #   if @user.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def create
-    @user = User.new(params[:user])
-    if @user.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
+  # def edit
+  #   @user = User.find(params[:id])
+  # end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-  end
+  # def update
+  #   @user = User.find(params[:id])
+  # end
 
   # def destroy
   # end
