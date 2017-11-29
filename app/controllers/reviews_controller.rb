e075019b29a6d2faf4cf0383d61d574e27ebcbf6
Joevin Canet <canetjoevin@gmail.com>
2017-11-29 14:10:02 +0100

    simple form in review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,12 @@
 class ReviewsController < ApplicationController
   def new
     @review = Review.new
+    @nanny_offer = NannyOffer.find(params[:nanny_offer_id])
   end
 
   def create
     @review = Review.new(review_params)
-    @nanny_offer = NannyOffer.find(params[:nany_offer_id])
+    @nanny_offer = NannyOffer.find(params[:nanny_offer_id])
     @review.user = current_user
     @review.nanny_offer = @nanny_offer
     if @review.save
