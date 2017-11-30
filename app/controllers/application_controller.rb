a5b5189d8f4b1dfea791f3c5fa0232d3dc97f524
Florian Herlicq <fherlicq@gmail.com>
2017-11-30 11:28:33 +0100

    solved a few bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :age, :gender, :profile_picture, :appartment_equipment, :appartment_rules, :appartment_address, :appartment_photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :age, :gender, :profile_picture, :appartment_equipment, :appartment_rules, :appartment_address, :appartment_photo, :short_description])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :phone_number, :age, :gender, :profile_picture, :appartment_equipment, :appartment_rules, :appartment_address, :appartment_photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :first_name, :last_name, :phone_number, :age, :gender, :profile_picture, :appartment_equipment, :appartment_rules, :appartment_address, :appartment_photo, :short_description])
   end
 end
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -3,4 +3,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   # Remove everything else
+
+  # def default_url
+  #   "/assets/images/no_appartment_photo.jpg"
+  # end
 end
